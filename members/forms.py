8093576815af221d9463045a30bb93b243a9323d
hamidur0x <hamidursohan10@gmail.com>
2026-08-26 16:50:04 +0600

    done1

--- members/forms.py
+++ members/forms.py
@@ -28,7 +28,7 @@ class MemberRegistrationForm(forms.ModelForm):
             "full_name",
             "profile_photo",
             "student_id",
-            "department",
+            "upodol",
             "technology",
             "semester",
             "session",
